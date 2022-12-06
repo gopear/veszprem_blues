4d995d7dee87f5020a486077146401dde562d747
kovapatrik <kovapatrik@gmail.com>
2022-12-07 00:21:55 +0100

    feat: test workaround

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,15 +1,30 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { GatsbyBrowser } from 'gatsby';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SSRProvider } from 'react-bootstrap';
 import "./src/styles/global.css"
+
+const Wrapper = ({ element } : { element : React.ReactElement<any, string | React.JSXElementConstructor<any>>}) => {
+  useEffect(() => {
+      
+    const documentHeight = () => {
+      const doc = document.documentElement
+      doc.style.setProperty('--doc-height', `${window.innerHeight}px`)
+     }
+     
+    window.addEventListener('resize',documentHeight)
+    documentHeight()
+  }, [])
+
+  return (
+    <SSRProvider>
+      {element}
+    </SSRProvider>
+  )
+}
 
 export const wrapRootElement: GatsbyBrowser["wrapRootElement"] = ({
     element,
   }) => {
-    return (
-      <SSRProvider>
-        {element}
-      </SSRProvider>
-    )
+    return <Wrapper element={element}/>
   }
