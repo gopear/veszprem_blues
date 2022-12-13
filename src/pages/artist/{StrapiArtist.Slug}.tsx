5f3de014ceda2e0b4ba206c17b632ccd969531c9
kovapatrik <kovapatrik@gmail.com>
2022-12-13 02:09:49 +0100

    feat: artist performance

--- src/pages/artist/{StrapiArtist.Slug}.tsx
+++ src/pages/artist/{StrapiArtist.Slug}.tsx
@@ -1,11 +1,18 @@
 import { graphql, HeadProps, PageProps } from 'gatsby'
 import { GatsbyImage } from 'gatsby-plugin-image'
 import React from 'react'
-import { Col, Container, Row } from 'react-bootstrap'
+import { Col, Container, Row, Stack } from 'react-bootstrap'
 import Layout from '../../components/layout'
 import { SEO } from '../../components/seo'
 import UnderConstruction from '../../components/under_construction'
 import * as styles from "../../styles/artist.module.css"
+
+interface Performance {
+  PerformanceDate: string;
+  PerformanceDuration: string;
+  PerformanceStart: string;
+  Stage: string;
+}
 
 const Artist = ({ data }: PageProps<Queries.ArtistPageQuery>) => {
 
@@ -24,6 +31,15 @@ const Artist = ({ data }: PageProps<Queries.ArtistPageQuery>) => {
 
   const description = data.strapiArtist?.Description?.data ? data.strapiArtist.Description.data.childMarkdownRemark!.html! : ''
 
+
+  let performance = undefined;
+
+  if (data.strapiArtist?.Performance) {
+    performance = Array.from(data.strapiArtist?.Performance);
+    performance = performance.filter(p => p?.PerformanceDate && p?.PerformanceDuration && p?.PerformanceStart && p?.Stage) as Performance[]
+    performance.sort((a,b) => a.PerformanceDate.localeCompare(b.PerformanceDate!) || a.PerformanceStart.localeCompare(b.PerformanceStart))
+  }
+
   return (
     <Layout>
       <Container fluid>
@@ -40,6 +56,20 @@ const Artist = ({ data }: PageProps<Queries.ArtistPageQuery>) => {
           <Row className={styles.data_wrapper}>
             <Col xs={12} sm={10} lg={data.strapiArtist?.Spotify ? 7 : 10}>
               <div className={styles.description} dangerouslySetInnerHTML={{__html: description}}/>
+              {performance ? 
+                <div className={styles.performance_wrapper}>
+                  {performance.map((p, idx) => (
+                    <Stack key={idx} className={styles.performance}>
+                      <span className={styles.date}>{p.PerformanceDate}</span>
+                      <span>{p.PerformanceStart.slice(0, -3)}</span>
+                      <span>{p.Stage}</span>
+                    </Stack>
+                  ))
+                  }
+                </div>
+                :
+                null
+              }
             </Col>
             { spoti && 
               <Col xs={12} sm={10} lg={3}>
@@ -50,6 +80,16 @@ const Artist = ({ data }: PageProps<Queries.ArtistPageQuery>) => {
               </Col>
             }
           </Row>
+          { performance ? 
+            <Row>
+              <Col xs={12} sm={10}>
+                
+              </Col>
+            </Row>
+            :
+            null
+          }
+          
         </>
         :
           <UnderConstruction url={data.wip!.publicURL}/>
@@ -100,6 +140,12 @@ query ArtistPage($language: String!,  $Slug: String!) {
     Nationality
     Slug
     Spotify
+    Performance {
+        PerformanceDate
+        PerformanceDuration
+        PerformanceStart
+        Stage
+    }
   }
   wip:file(name: {eq: "vbf_underconstruction"}, sourceInstanceName: {eq: $language}) {
     publicURL
