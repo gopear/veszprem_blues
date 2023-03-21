9c41622e581cd836f489ccdc2b3c3f492aed57db
kovapatrik <kovapatrik@gmail.com>
2023-03-21 14:35:14 +0100

    feat: ws done

--- src/pages/workshops.tsx
+++ src/pages/workshops.tsx
@@ -1,10 +1,19 @@
 import { graphql, HeadProps, PageProps } from 'gatsby'
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Col, Container, Row } from 'react-bootstrap'
 import { Trans } from 'react-i18next'
 import Layout from '../components/layout'
 import { SEO } from '../components/seo'
 import * as styles from '../styles/workshops.module.css'
+
+function getYoutubeId(url: string) {
+  const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+  const match = url.match(regExp);
+
+  return (match && match[2].length === 11)
+    ? match[2]
+    : null;
+}
 
 const Workshops = ({ data }: PageProps<Queries.WorkshopsPageQuery>) => {
 
@@ -14,24 +23,37 @@ const Workshops = ({ data }: PageProps<Queries.WorkshopsPageQuery>) => {
 
     return (
         <Layout>
-            <Container fluid>
+            <Container fluid className={styles.container}>
                 {dates.map(d => {
                     const date_workshops = workshops.filter(w => w.Date === d); 
                     return (
                         date_workshops.length > 0 ?
-                            <div key={d}>
-                                <div>
+                            <div key={d} className={styles.day_wrapper}>
+                                <div className={styles.day}>
                                     <h2><Trans>{days[dates.indexOf(d)]}</Trans></h2>
-                                    <h3>{d}</h3>    
+                                    <h3>{d}</h3>
                                 </div>
                                 <div>
                                     {date_workshops.map(w => {
                                         return (
-                                            <div key={w.Name} className={styles.workshop_wrapper}>
-                                                <h4>{w.Name}</h4>
-                                                <div dangerouslySetInnerHTML={{__html: w.Description!.data!.childMarkdownRemark!.html!}}></div>
-                                                <a href={w.Link!}>Jegy</a>
-                                            </div>
+                                            <Row key={w.Name} className={styles.workshop_wrapper}>
+                                                <Col className={styles.ws_content_wrapper} xl={12} xxl={6}>
+                                                  <h4 className={styles.ws_title}>{w.Name}</h4>
+                                                  <div dangerouslySetInnerHTML={{__html: w.Description!.data!.childMarkdownRemark!.html!}} className={styles.ws_content}/>
+                                                  <div className={styles.btn_wrapper}>
+                                                    <a className={styles.jegyek} href={w.TicketLink!}>
+                                                        <Trans>MEGVESZEM</Trans>
+                                                    </a>
+                                                  </div>
+                                                </Col>
+                                                <Col className={styles.media_col} xl={12} xxl={5}>
+                                                <iframe className={`embed-responsive-item ${styles.media}`} src={`https://www.youtube.com/embed/${getYoutubeId(w.YoutubeLink!)}`}
+                                                        width="300" height="380"
+                                                        title={`${w.Name} YouTube video`}
+                                                        allow="encrypted-media"
+                                                        style={{ borderRadius: 10 }} />
+                                                </Col>
+                                            </Row>
                                         )
                                     })}
                                 </div>
@@ -66,7 +88,8 @@ query WorkshopsPage($language: String!) {
   allStrapiProgram {
     nodes {
       Name
-      Link
+      TicketLink
+      YoutubeLink
       Description {
         data {
           childMarkdownRemark {
